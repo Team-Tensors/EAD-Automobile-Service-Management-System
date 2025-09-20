82ad6d45290c3eb8b297b9801c569e1493c2ffa8
lakshanabenayake <lakshanabenayake@gmail.com>
2025-09-21 02:52:18 +0530

    created User class

--- backend/src/main/java/com/ead/backend/entity/User.java
+++ backend/src/main/java/com/ead/backend/entity/User.java
@@ -1,4 +1,33 @@
 package com.ead.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import javax.management.relation.Role;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,nullable = false)
+    private String username;
+
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password; // Hashed
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 }
