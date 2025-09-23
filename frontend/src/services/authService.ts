58680d43ebb4aec65f2942384649d63d84dc31dc
lakshanabenayake <lakshanabenayake@gmail.com>
2025-09-23 11:33:22 +0530

    implemenetd auth service

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,32 @@
+import api from "../utill/apiUtils";
+
+// User registration data interface
+export interface RegisterCustomerData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  phone: string;
+}
+
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export const authService = {
+
+    //login user
+    Login: async (credentials: LoginCredentials) => {
+            const response = await api.post("/auth/login", credentials);
+            return response.data;
+        },
+
+    // Register new customer
+    Register: async (customerData: RegisterCustomerData) => {
+            const response = await api.post("/auth/register", customerData);
+            return response.data;
+    },
+};
+
+
