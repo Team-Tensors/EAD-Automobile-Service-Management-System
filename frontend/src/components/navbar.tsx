d9a42c92edcbff058981fcb1c720d8eab2242cc4
Bhashitha  Samarasinghe <bhashithasamarasinghe@gmail.com>
2025-10-12 12:49:44 +0530

    feat(ui): reintroduce NavBar and Footer components with updated imports

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Button } from './button'
+import { Button } from './ui/button'
 
 const NavBar: React.FC = () => {
   return (
