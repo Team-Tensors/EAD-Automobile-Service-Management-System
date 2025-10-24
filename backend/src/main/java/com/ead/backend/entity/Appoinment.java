ae94b5248b0acfaccd1782dff5a7771016b709b1
Swetha Nethmi <snethmi18@gmail.com>
2025-10-24 22:39:17 +0530

    feat: create Appoinment entity

---
+++ backend/src/main/java/com/ead/backend/entity/Appoinment.java
@@ -0,0 +1,44 @@
+package com.ead.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "appointment")
+@Data
+@NoArgsConstructor
+@Getter
+@Setter
+public class Appoinment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "service_type_id", nullable = false)
+    private ServiceType serviceType;
+
+    @Column(nullable = false)
+    private LocalDateTime appoinmentDate;
+
+    @Column(nullable = false)
+    private String status; //e.g., "PENDING", "CONFIRMED", "COMPLETED", "CANCELLED"
+
+    @Column
+    private String description;
+
+}
