9fadf9a520b3d6ff682f20540bcace10bb302010
Bhashitha  Samarasinghe <bhashithasamarasinghe@gmail.com>
2025-10-25 21:35:30 +0530

    Add Services component to HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar/Narbar";
 import Hero from "@/components/HomePage/Hero";
 import Features from "@/components/HomePage/Features";
+import Services from "@/components/HomePage/Services";
 import Footer from "@/components/Footer/Footer";
 
 const HomePage = () => {
@@ -10,6 +11,8 @@ const HomePage = () => {
       <main>
         <Hero />
         <Features />
+        <Services />
+        
       </main>
       <Footer />
     </div>
