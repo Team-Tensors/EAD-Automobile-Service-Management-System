03e67751a07e450b2b0ce1aa6b4dda16252dc38a
Swetha Nethmi <snethmi18@gmail.com>
2025-10-25 21:16:35 +0530

    feat: update appoinment service and repo

--- backend/src/main/java/com/ead/backend/repository/AppointmentRepository.java
+++ backend/src/main/java/com/ead/backend/repository/AppointmentRepository.java
@@ -2,9 +2,11 @@ package com.ead.backend.repository;
 
 import com.ead.backend.entity.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
     List<Appointment> findByUserId(Long userId);
+
+    // Find appointments assigned to a specific employee
+    List<Appointment> findByAssignedEmployeesId(Long employeeId);
 }
