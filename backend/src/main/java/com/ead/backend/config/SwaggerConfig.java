a7283a5900a6c5bf6d218eb1c92767f2da222b6d
rashmithachamikara <rashmitha.chamickara@gmail.com>
2025-10-28 08:19:15 +0530

    Update swagger config

--- backend/src/main/java/com/ead/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/ead/backend/config/SwaggerConfig.java
@@ -2,6 +2,11 @@ package com.ead.backend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+//import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,8 +19,27 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("EAD API")
-                        .description("Automobile Service Management System API")
-                        .version("1.0"));
+                        .title("DriveCare API")
+                        .description("""
+                                A comprehensive REST API for DriveCare - your complete automobile service management solution.
+                                
+                                **Authentication:**
+                                Use the `/auth/login` endpoint to obtain a JWT token, 
+                                then include it in the Authorization header as `Bearer <token>`
+                                """)
+                        .version("1.0.0")
+//                        .contact(new Contact()
+//                                .name("DriveCare Development Team")
+//                                .email("support@drivecare.com"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("JWT Authorization header using the Bearer scheme. Example: \"Bearer {token}\"")));
     }
 }
