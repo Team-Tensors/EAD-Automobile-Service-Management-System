e043edabdf308f845d170fb6eeb4d6ea8dbd6aff
Swetha Nethmi <snethmi18@gmail.com>
2025-10-30 21:42:36 +0530

    feat: add service or modifications repo dto and controller

--- backend/src/main/java/com/ead/backend/repository/ServiceOrModificationRepository.java
+++ backend/src/main/java/com/ead/backend/repository/ServiceOrModificationRepository.java
@@ -1,9 +1,13 @@
 package com.ead.backend.repository;
 
+import com.ead.backend.entity.AppointmentType;
 import com.ead.backend.entity.ServiceOrModification;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface ServiceOrModificationRepository extends JpaRepository<ServiceOrModification, Long> {
+    List<ServiceOrModification> findByType(AppointmentType type);
     Optional<ServiceOrModification> findByUniqueName(String uniqueName);
 }
