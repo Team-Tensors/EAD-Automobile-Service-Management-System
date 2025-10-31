adfe4a4d6308c8524d522d109eb84436795ad939
Swetha Nethmi <snethmi18@gmail.com>
2025-10-31 13:17:02 +0530

    feat: remove modification type entitiy and repo

--- backend/src/main/java/com/ead/backend/repository/ModificationTypeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ead.backend.repository;
-
-import com.ead.backend.entity.ModificationType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ModificationTypeRepository extends JpaRepository<ModificationType, Long> {}
