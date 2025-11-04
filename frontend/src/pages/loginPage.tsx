56831a28cf17625d3ad41b35a95dbc69319a7734
Bhashitha  Samarasinghe <bhashithasamarasinghe@gmail.com>
2025-11-05 00:14:13 +0530

    feat: update Footer and forms for improved styling and layout consistency

--- frontend/src/pages/loginPage.tsx
+++ frontend/src/pages/loginPage.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "../components/loginPage/login-form"
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center bg-zinc-950 p-6 md:p-10">
       <div className="w-full max-w-md md:max-w-4xl">
         <LoginForm />
       </div>
